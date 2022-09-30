c4db79fcfb0bd80e3157127d34e71f5bf5e80d76
LAPTOP-DRN25HR2\jxmlx <531476@escg.ac.uk>
2022-09-30 19:06:00 +0100

    final code

--- Assignment 1/Program.cs
+++ Assignment 1/Program.cs
@@ -16,6 +16,7 @@ string yn = Console.ReadLine();
 if (yn == "y" || yn == "Y")
 {
     Console.WriteLine("Good I will give you the first hint into finding him!");
+    Console.Clear();
 }
 else if (yn == "n"|| yn == "N")
 {
@@ -27,24 +28,172 @@ else if (yn == "n"|| yn == "N")
 
 Console.WriteLine("");
 Thread.Sleep(1500);
-Console.WriteLine("He is hiding in one of three places; where did you want to look first?");
+Console.WriteLine("He is hiding in one of two places; where did you want to look first?");
 Thread.Sleep(1500);
-Console.WriteLine("(1) Forbidden forest, (2) London - near the Ministry of Magic, (3) Falmouth - his birth home");
+Console.WriteLine("(1)  Falmouth - his birth home, (2) Forbidden forest");
 string pl1 = Console.ReadLine();
-if (pl1 == "1")
+if (pl1 == "2")
 {
-    Console.WriteLine("You look in the forbidden forest");
+    Thread.Sleep(1500);
+    Console.WriteLine("");
+    Console.WriteLine("You look in the forbidden forest..");
+    Thread.Sleep(3000);
+    Console.WriteLine("You find a tree that looked suspicious..");
+    Thread.Sleep(3000);
+    Console.WriteLine("It seems to have a small caved bit into it..");
+    Thread.Sleep(3000);
+    Console.WriteLine("There appears to be a light coming from the cave, the tree seems to be enchanted.");
+    Thread.Sleep(3000);
+    Console.WriteLine("You use the charm 'Appare Vestigium' which reveals traces of magic...");
+    Thread.Sleep(3000);
+    Console.WriteLine("The charm reveals that there is magic being used on the tree.");
+    Thread.Sleep(3000);
+    Console.WriteLine("You take a closer look and discover a secret area..");
+    Thread.Sleep(3000);
+    Console.WriteLine("You enter the secret cave and notice something strange..");
+    Thread.Sleep(3000);
+    Console.WriteLine("Someone defiently hides here so you get your wand out while looking around..");
+    Thread.Sleep(3000);
+    Console.WriteLine("You hear something move behind a rock, you get you wand ready to cast a spell..");
+    Thread.Sleep(3000);
+    Console.WriteLine("An unknown voice hissed,'I was expecting someone to come find me! I hope your ready to fight!'");
+    Thread.Sleep(10000);
+    Console.Clear();
+    Thread.Sleep(3000);
+    Console.WriteLine("Final battle loading...");
+}
+else if (pl1 == "1")
+{
+    Thread.Sleep(1500);
+    Console.WriteLine("");
+    Console.WriteLine("You go to Falmouth to find Valdis' birth home before being abandonned by his family.");
+    Thread.Sleep(3000);
+    Console.WriteLine("You look around the small town to try and find any signs of him or any suspicious..");
+    Thread.Sleep(3000);
+    Console.WriteLine("You come across a small little cottage where Valdis was homed until the age of 5..");
+    Thread.Sleep(3000);
+    Console.WriteLine("Do you want to investigate the cottage? y/n ");
+    string yn2 = Console.ReadLine();
+    if (yn2 == "y" || yn2 == "Y")
+    {
+        Console.WriteLine("");
+        Thread.Sleep(3000);
+        Console.WriteLine("You look inside the cottage, theres no one there but you look for clues..");
+        Thread.Sleep(3000);
+        Console.WriteLine("You find an old picture on the floor that has been burnt, it was a family photo of Valdis and his parents..");
+        Thread.Sleep(3000);
+        Console.WriteLine("Valdis must of burnt this picture when he killed his muggle parents..");
+        Thread.Sleep(3000);
+        Console.WriteLine("You look around a bit more but find nothing else so you exit the cottage..");
+        Console.WriteLine("");
+        Thread.Sleep(3000);
+        Console.WriteLine("You have exited the cottage and looking around outside..");
+        Thread.Sleep(3000);
+        Console.WriteLine("You go to look at something that seems suspicious..");
+        Thread.Sleep(3000);
+        Console.WriteLine("Theres a person watching you in a black cloak..");
+        Thread.Sleep(3000);
+        Console.WriteLine("The person slowly walks up to you..");
+        Thread.Sleep(3000);
+        Console.WriteLine("He approaches you and whispers 'I know your looking for Valdis, Valdis knows this too'..");
+        Thread.Sleep(3000);
+        Console.WriteLine("He adds 'If you want to find him, hes no where near here. Theres a forest he used to go to a lot'..");
+        Thread.Sleep(3000);
+        Console.WriteLine("The only place you think of is the forbidden forest so you decide to go look there..");
+
+        Thread.Sleep(4000);
+        Console.Clear();
+        Console.WriteLine("");
+        Console.WriteLine("You look in the forbidden forest..");
+        Thread.Sleep(3000);
+        Console.WriteLine("You find a tree that looked suspicious..");
+        Thread.Sleep(3000);
+        Console.WriteLine("It seems to have a small caved bit into it..");
+        Thread.Sleep(3000);
+        Console.WriteLine("There appears to be a light coming from the cave, the tree seems to be enchanted.");
+        Thread.Sleep(3000);
+        Console.WriteLine("You use the charm 'Appare Vestigium' which reveals traces of magic...");
+        Thread.Sleep(3000);
+        Console.WriteLine("The charm reveals that there is magic being used on the tree.");
+        Thread.Sleep(3000);
+        Console.WriteLine("You take a closer look and discover a secret area..");
+        Thread.Sleep(3000);
+        Console.WriteLine("You enter the secret cave and notice something strange..");
+        Thread.Sleep(3000);
+        Console.WriteLine("Someone defiently hides here so you get your wand out while looking around..");
+        Thread.Sleep(3000);
+        Console.WriteLine("You hear something move behind a rock, you get you wand ready to cast a spell..");
+        Thread.Sleep(3000);
+        Console.WriteLine("An unknown voice hissed,'I was expecting someone to come find me! I hope your ready to fight!'");
+        Thread.Sleep(10000);
+        Console.Clear();
+        Thread.Sleep(3000);
+        Console.WriteLine("Final battle loading...");
+
+
+    }
+    else if (yn2 == "n" || yn2 == "N")
+    {
+        Console.WriteLine("");
+        Thread.Sleep(3000);
+        Console.WriteLine("You decided not to look in the cottage..");
+        Thread.Sleep(3000);
+        Console.WriteLine("You go to look at something that seems suspicious..");
+        Thread.Sleep(3000);
+        Console.WriteLine("Theres a person watching you in a black cloak..");
+        Thread.Sleep(3000);
+        Console.WriteLine("The person slowly walks up to you..");
+        Thread.Sleep(3000);
+        Console.WriteLine("He approaches you and whispers 'I know your looking for Valdis, Valdis knows this too'..");
+        Thread.Sleep(3000);
+        Console.WriteLine("He adds 'If you want to find him, hes no where near here. Theres a forest he used to go to a lot'..");
+        Thread.Sleep(3000);
+        Console.WriteLine("The only place you think of is the forbidden forest so you decide to go look there..");
+        Thread.Sleep(4000);
+
+        Thread.Sleep(3000);
+        Console.Clear();
+        Console.WriteLine("");
+        Console.WriteLine("You look in the forbidden forest..");
+        Thread.Sleep(3000);
+        Console.WriteLine("You find a tree that looked suspicious..");
+        Thread.Sleep(3000);
+        Console.WriteLine("It seems to have a small caved bit into it..");
+        Thread.Sleep(3000);
+        Console.WriteLine("There appears to be a light coming from the cave, the tree seems to be enchanted.");
+        Thread.Sleep(3000);
+        Console.WriteLine("You use the charm 'Appare Vestigium' which reveals traces of magic...");
+        Thread.Sleep(3000);
+        Console.WriteLine("The charm reveals that there is magic being used on the tree.");
+        Thread.Sleep(3000);
+        Console.WriteLine("You take a closer look and discover a secret area..");
+        Thread.Sleep(3000);
+        Console.WriteLine("You enter the secret cave and notice something strange..");
+        Thread.Sleep(3000);
+        Console.WriteLine("Someone defiently hides here so you get your wand out while looking around..");
+        Thread.Sleep(3000);
+        Console.WriteLine("You hear something move behind a rock, you get you wand ready to cast a spell..");
+        Thread.Sleep(3000);
+        Console.WriteLine("An unknown voice hissed,'I was expecting someone to come find me! I hope your ready to fight!");
+        Thread.Sleep(10000);
+        Console.Clear();
+        Thread.Sleep(3000);
+        Console.WriteLine("Final battle loading...");
+    }
 }
 
 
 
 //Final Battle - Valdis
 
-int Vhealth = 150;
+int Vhealth = 120;
 int Ihealth = 100;
 Random Def = new Random();
 Random Att = new Random();
 
+Thread.Sleep(10000);
+Console.Clear();
+Console.WriteLine("");
 Console.WriteLine("You are in the final battle with Valdis, you have two options to either defend or attack.");
 Thread.Sleep(1500);
 Console.WriteLine("Attack generates a random number where it will take off some of Valdis' health depending on the number");
